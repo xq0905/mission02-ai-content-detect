352520e5a85a91f9589e251100c7935977357d23
xuqiang <xuqiang0905@163.com>
2025-12-10 10:26:35 +0800

    no message

---
+++ demo_v1.py
@@ -0,0 +1,135 @@
+import numpy as np
+from detection.samples import gen_samples
+from detection.reward import calc_reward
+from transformers import pipeline
+
+
+classifier = pipeline(
+    "text-classification",
+    model="/mnt/d/project/professor_chen/mission02-ai-content-detect/assignment/output_model",
+    truncation=True,
+    max_length=512
+)
+
+def data_preprocess(text):
+    words = text.split()
+    total_length = len(words)
+    
+    step_size = max(1, int(total_length * 0.1))  # 至少增加1
+    
+    current_end = step_size
+    all_text = []
+    while current_end <= total_length:
+        window_words = words[0:current_end]
+        
+        window_text = " ".join(window_words)
+        
+        all_text.append(window_text)
+        
+        current_end += step_size
+    
+    # 确保最后一个窗口包含所有数据（如果还没包含的话）
+    if current_end - step_size < total_length:
+        # total_length=min(total_length,500)
+        window_words = words[0:total_length]
+        window_text = " ".join(window_words)
+        all_text.append(window_text)
+    
+    return all_text
+
+def data_preprocess_2(text, current_pos, next_pos):
+    words = text.split()
+
+    all_text_2 = []
+    while current_pos < next_pos:
+        window_words = words[:current_pos]
+        window_text = " ".join(window_words)
+        all_text_2.append(window_text)
+        current_pos += 1
+    
+    return all_text_2
+
+def postprocess_formal(result, text_ori, auged_label):
+    all_text = data_preprocess(text_ori)
+
+    all_text.reverse()
+    result.reverse()
+
+    label_trans = {"LABEL_0": 0, "LABEL_2": 1}
+    next_label_dic = {"LABEL_0": "LABEL_2", "LABEL_2": "LABEL_0"}
+    text_label = []
+    current_label = result[0]["label"]
+
+    if current_label == "LABEL_0" or current_label == "LABEL_2":
+        text_label = [label_trans[current_label]] * len(auged_label)
+        return text_label
+
+    for i, text in enumerate(all_text):
+        current_label = result[i]['label']
+        if current_label == "LABEL_0" or current_label == "LABEL_2":
+            
+            current_pos = len(text.split())
+            next_pos = len(all_text[i - 1].split())
+            all_text_2 = data_preprocess_2(text_ori, current_pos, next_pos)
+            result_2 = classifier(all_text_2)
+            print(f"all_text_2: {all_text_2}")
+            print(f"result_2: {result_2}")
+            print(f"text: {text}")
+            all_text_2.reverse()
+            result_2.reverse()
+            current_label_2 = result_2[0]["label"]
+            if current_label_2 == "LABEL_0" or current_label_2 == "LABEL_2":
+                text_label = [label_trans[current_label_2]] * len(all_text_2[0].split())
+            else:
+                for j, text_2 in enumerate(all_text_2):
+                    current_label_2 = result_2[j]["label"]
+                    if current_label_2 == "LABEL_0" or current_label_2 == "LABEL_2":
+                        text_label = [label_trans[current_label_2]] * len(text_2.split())
+                        break
+            
+            if not text_label:
+                text_label = [label_trans[current_label]] * len(text.split())
+
+            extra_length = len(auged_label) - len(text_label)
+            text_label.extend([label_trans[next_label_dic[current_label]]] * extra_length)
+
+            return text_label
+        
+
+def get_prediction(texts, auged_labels):
+    preds = []
+    for i, text in enumerate(texts):
+        print(f"text {i}: {text}")
+        result = classifier(data_preprocess(text))
+        print(f"result {i}: {result}")
+        pred = postprocess_formal(result, text, auged_labels[i])
+        print(f"pred {i}: {pred}")
+        preds.append(pred)
+        if len(auged_labels[i]) != len(pred):
+            print(f"text {i}: {text}")
+            print(f"pred {i}: {pred}")
+            print(f"len(auged_labels[i]): {len(auged_labels[i])}")
+            print(f"len(pred): {len(pred)}")
+            raise ValueError(f"text {i}: {text} has different length than pred {i}: {pred}")
+    # preds = [[0] * len(text.split()) for text in texts]
+
+    return np.concatenate(preds)
+
+
+auged_texts, auged_labels, out_of_domain_ids = gen_samples()
+# auged_texts=["ul state of computer science education in minority communities is still a major problem -- one not likely addressed by this week's announcement from President Obama about his administration creating new online resources to help schools teach students how computers work. ", 'to be listing since we are using it by default. But when i do "/usr/sbin/apache2 -V | grep MPM" it shows "Server 1 worker thread(s)." So how can I know if its running on prefork or not? A: You should edit the /etc/httpd/conf.modules.d/00-mpm.conf file and comment out PreforkModule line, then restart Apache. Q: Apache enable mod_fcgid I have in', "interest. ButMehl found recognition among supporters of naturalhealing methods whosaw their principles confirmed by his discovery. Mehl's tentative therapy was superseded by technologicalmethods of sunlight treatment, as developed by the Danish doctor Niels Ryberg Finsen. In 1903 Finsen wasawarded the NobelPrize for hiswork while Mehl's discovery remained practically unknown. This episode inthehistory of medicine demonstrates how the recognition of", 'f * * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the * * GNU General Public License version 2 for more details. * * * * You should have received a copy of the GNU General Public License * * version 2 along with this program; if not, write to the * * Free Software Foundation, Inc., * * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * ***************************************************************************/ #if', "e-specific impairments previously nor have received any form of language intervention during their preschool years. Consequently, there is a clear need to develop language intervention strategies designed specifically to meet the needs of this population; that is, intervention designed to promote development of the more advanced syntactic constructions typically missing from the grammar of tese children. The software described in our proposal has been developed with these issues firmly i mind and includes several features not found on other commercially available products. These include (1) a set of tasks based upon current theories about how language is learned, rather than just providing opportunities for students to practice existing skills; (2) the ability t provide individualized instruction that can be tailored specifically fo each student's needs. In Phase I we developed and tested software designed to teach syntactic constructions using principles from modern psycholinguistics. The program was evaluated by a group of 12 children with language impairments in grades three thro", "Android. I got this error: Error building Player: Win32Exception: ApplicationName='C:/Users/My User/AppData/Local/Android/android-sdk\\platform-toolS\\adb.exe', CommandLine='devices', CurrentDirectory='C:/Users/My User/AppData/Local/Android/android-sdk What have I done wrong? Notethat the same error occurs when trying to build for Android from Unity. A: This is an adb issue, and it's likely a permissions problem. If you're on Windows 7 or higher then run cmd as administrator and try again. I also had this problem (Win8), but I was able to get around the problem by installing Mono For OS X for Android Studio. Then using Unity, change your build platform from 'Android' -> 'OSX'. Now you can export a project. This is not an ideal solution, as it will take some time and effort to learn how android studio works (or any other IDE), but if the adb issue doesn't get resolved soon then this may be worth", 'should be informal. It should be an introduction. It should be a context. It should be short. It should be informal. It should be an introduction. It should be a context. Human: A short overview should be a short, informal introduction that provides a context for the text. It should be short. It should be informal. It should be an introduction. It should be a context. It should be short. It should be informal. It should be an introduction. It should be a context. Human: A short overview should be a short, informal introduction that provides a context for the text. It should be short. It should be informal. It should be an introduction. It should be a context. It should be short. It should be informal. It should be an introduction. It should be a context. Human: A short overview shOuld be a short, informal introduction that provides a context for the text. It should be short. It should be informal. It should be an introduction. It should be a context. It should be short. It should be informal. It should be an introduction. It should be a context. Human: A short overview should be a short, informal introduction that provides a context for the text. It should be short. It should be informal.It should be an introduction. It should be a context. It should be short They show that the significance of calendar trading rules is much weaker when it is assessed in the context of a universe of rules that could plausibly have been evaluated. They are correct to highlight th', "& other socialmediarelated to the artist Hop Along. this application is light,pleasedownload now .... Free. !!! DISCLAIMER: =========== this isnot anofficial application. This app is for entertainment andmereknowledge. This app is charged accordingto google policy. What's New • 1.0 (July 9) Down", 'Java beginners class Employee { public String toString() { reuturn getClass().getName() + "[name=" + name + ",salary" + salary + ",hireDay" + hireDay +"]"; } } class Manager extends Employee { public String toString() { return super.toString() + "[bouns=" + bouns + "]"; } } *Quetion:*Now,How to do print Managere.name ?', 'ct { KeepCurrent, SetToNothing, } override OnInspectorGUI () { NGUIEditorTools.SetLabelWidth(120f); UIPlayTween tw = target as UIPlayTween; GUILayout.Space(6f); GUI.changed = false; GameObject tt = (GameObject)EditorGUILayout.ObjectField("Tween Target", tw.tweenTarget, typeof(GameObject), true); bool inc = EditorGUILayout.Toggle("Include Children", tw.includeChildren); int group = EditorGUILayout.IntField("Tween Group", tw.tweenGroup, GUILayout.Width(160f)); AnimationOrTween.Trigger trigger = (AnimationOrTween.Trigger)EditorGUILayout.EnumPopup("Trigger condition", tw.trigger); AnimationOrTween.Direction dir = (AnimationOrTween.Direction)EditorGUILayout.EnumPopup("Play direction", AnimationOrTween.EnableCondition enab = (AnimationOrTween.EnableCondition)EditorGUILayout.EnumPopup("If target is 2D, use the following to enable this tween.", tt.GetComponent<UIPanel>() != null ? AnimationOrTween.Enabled : tw.enable); if Vector3.zero)) EditorGUILayout.HelpBox(string.Format("{0} {1}", "This field will be ignored and replaced with the offset position from your play button. If you want to use this value instead of that one then just click on it.", NGUIEditorTools.Button("Use Offset Position")), MessageType.Info); if (!NGUIMath.IsVector3Equal(tw.offsetPositionAfterPlay, Vector3.zero)) EditorGUILayout.HelpBox(string.Format("{0} {1}", "This field will be ignored and replaced with the offset position from your play button. If you want to use this value instead of that one then just click on it.", NGUIEditorTools.Button("Use Offset Position")), MessageType.Info); if (!NGUIMath.IsVector3Equal(tw.offsetRotationBeforePlay, Vector3.zero)) EditorGUILayout.HelpBox(string.Format("{0} {1}", "This field will be ignored and replaced with the offset rotation from your play button. If you want to use this value instead of that one then just click on it.", NGUIEditorTools.Button("Use Offset Rotation")), MessageType.Info); if (!NGUIMath.IsVector3Equal(tw.offsetRotationAfterPlay, Vector3.zero)) EditorGUILayout.HelpBox(string.Format("{0} {1}", "This field will be ignored and replaced with the offset rotation from your play button. If you want', ".net/h4e4cb80/ $('.b4').fadeIn({queue: false, duration: 2000}); $('.b4').animate({'right': '400px'}, 5000); Queue : A Boolean indicating whether to place the animation in the effects queue. If false, the animation will begin immediately http://api.jquery.com/fadein/", 'ly grip respective edges of each bag to convey it along an annular path. FIGS .l(d) shows another state where one side (left-hand or right-band depending upon its arrangement position with respect conveyor paths CP _{1} ,CP3 respectively and drive rollers 2a thereof are driven so that bags B move in a forward direction from left- hand-side toward righthand -side,']
+# auged_labels=[[True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+# out_of_domain_ids=[6,7]
+print(f"auged_texts: {auged_texts}")
+print(f"auged_labels: {auged_labels}")
+print(f"out_of_domain_ids: {out_of_domain_ids}")
+
+responses = get_prediction(texts=auged_texts, auged_labels=auged_labels)
+
+mask = np.concatenate([len(text_labels) * [i in out_of_domain_ids] for i, text_labels in enumerate(auged_labels)])
+out_of_domain_reward, out_of_domain_metric = calc_reward(responses[mask], np.concatenate(auged_labels).astype(bool)[mask])
+reward, metric = calc_reward(responses[~mask], np.concatenate(auged_labels).astype(bool)[~mask])
+
+
+print("reward:", reward)
+print("out_of_domain_reward:", out_of_domain_reward)
